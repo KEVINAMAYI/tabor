f6f169f51bc3d63881d14104df09a865da0aac1d
ngugi_mukanga <dnmukanga@gmail.com>
2025-08-01 17:34:17 +0300

    constrained and nullable migration alignment

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -9,9 +9,7 @@ class Payment extends Model
 {
     use HasFactory;
 
-    protected $guarded = [
-        'id',
-    ];
+    protected $guarded = ['id'];
 
     /* -----------------------------------------------------------------
      |  Casts & dates
