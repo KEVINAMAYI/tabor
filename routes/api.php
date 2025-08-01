b150a91959a98e3e3842067eefe9882212d86192
ngugi_mukanga <dnmukanga@gmail.com>
2025-08-01 12:58:19 +0300

    fix api.php

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Http\Controllers\MpesaApi;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
-Route::post('/finance/confirmation', [MpesaApi::class, 'c2bConfirmation'])->withoutMiddleware([VerifyCsrfToken::class]);
-Route::post('/finance/validation', [MpesaApi::class, 'c2bValidation'])->withoutMiddleware([VerifyCsrfToken::class]);
-Route::post('/finance/callback', [MpesaApi::class, 'stkCallbackAction'])->withoutMiddleware([VerifyCsrfToken::class]);
-Route::post('/finance/trigger-stk', [MpesaApi::class, 'triggerStk'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::post('finance/confirmation', [MpesaApi::class, 'c2bConfirmation']);
+Route::post('finance/validation', [MpesaApi::class, 'c2bValidation']);
+Route::post('finance/callback', [MpesaApi::class, 'stkCallbackAction']);
+Route::post('finance/stk', [MpesaApi::class, 'initiateStk']);
