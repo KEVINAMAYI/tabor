9171f442438ef49212bef887bc9d26a6fffbf2cb
ngugi_mukanga <dnmukanga@gmail.com>
2025-08-01 12:09:34 +0300

    stk push

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::post('/finance/confirmation', [MpesaApi::class, 'c2bConfirmation'])->withoutMiddleware([VerifyCsrfToken::class]);
 Route::post('/finance/validation', [MpesaApi::class, 'c2bValidation'])->withoutMiddleware([VerifyCsrfToken::class]);
-Route::post('/finance/stk_response', [MpesaApi::class, 'stkCallbackAction'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::post('/finance/callback', [MpesaApi::class, 'stkCallbackAction'])->withoutMiddleware([VerifyCsrfToken::class]);
+Route::post('/finance/trigger-stk', [MpesaApi::class, 'triggerStk'])->withoutMiddleware([VerifyCsrfToken::class]);
