f276ff962bdbd47bd74b8862ef424025df89924a
kevin-amayi <kevinamayi20@gmail.com>
2025-08-07 22:35:59 +0300

    fix assets build

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -7,8 +7,8 @@
     @if (app()->environment('local'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-        <link rel="stylesheet" href="{{ asset('build/assets/app-BFrd5Ati.css') }}">
-        <script type="module" src="{{ asset('build/assets/app-l0sNRNKZ.js') }}"></script>
+        <link rel="stylesheet" href="{{ asset('build/assets/app-CSFslROV.css') }}">
+        <script type="module" src="{{ asset('build/assets/app-CFeSwWEE.js') }}"></script>
     @endif
 
 
