ce96dc730d2068b9dbf359840abc44022ea3ab33
ngugi_mukanga <dnmukanga@gmail.com>
2025-08-07 18:10:57 +0300

    fixed course_id nullable

--- routes/front-end/index.php
+++ routes/front-end/index.php
@@ -9,6 +9,6 @@ Volt::route('home', 'front-end.home')->name('front-end.home');
 Volt::route('about', 'front-end.about')->name('front-end.about');
 Volt::route('courses', 'front-end.courses')->name('front-end.courses');
 Volt::route('contact', 'front-end.contact')->name('front-end.contact');
-Volt::route('course-application/{course_id}', 'front-end.course-application')->name('front-end.course-application');
+Volt::route('course-application/{course_id?}', 'front-end.course-application')->name('front-end.course-application');
 Volt::route('faqs', 'front-end.faqs')->name('front-end.faqs');
 Volt::route('news', 'front-end.news')->name('front-end.news');
