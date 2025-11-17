88bc178ffaa2bf5da869a7562f06904c99dfb926
ngugi_mukanga <dnmukanga@gmail.com>
2025-11-18 00:41:10 +0300

    final changes for filter

--- resources/views/livewire/front-end/courses.blade.php
+++ resources/views/livewire/front-end/courses.blade.php
@@ -123,98 +123,97 @@ new #[Layout('components.layouts.app.frontend')] class extends Component {
     <section class="bg-light-gray pb-3 pb-md-7 pb-lg-12">
         <div class="container-fluid">
             <div class="row">
-                    <div class="col-3">
-                        @include('livewire.front-end.filters')
-                    </div>
-                    <div class="col-9">
-                        <!-- Check if there are any courses -->
-                        @if ($courses->isEmpty())
-                            <div class="col-12">
-                                <div class="alert alert-info text-center py-5 mt-4">
-                                    <h3 class="fw-bold">Currently, there are no courses available under that category.</h3>
-                                    <p class="fs-5">
-                                        We are constantly adding new courses. Please check back later or <a
-                                            href="{{ route('front-end.contact') }}" class="text-primary">contact us</a>
-                                        if you
-                                        have any questions.
-                                    </p>
-                                </div>
+                <div class="col-3">
+                    @include('livewire.front-end.filters')
+                </div>
+                <div class="col-9">
+                    <!-- Check if there are any courses -->
+                    @if ($courses->isEmpty())
+                        <div class="col-12">
+                            <div class="alert alert-info text-center py-5 mt-4">
+                                <h3 class="fw-bold">Currently, there are no courses available under that category.</h3>
+                                <p class="fs-5">
+                                    We are constantly adding new courses. Please check back later or <a
+                                        href="{{ route('front-end.contact') }}" class="text-primary">contact us</a>
+                                    if you
+                                    have any questions.
+                                </p>
                             </div>
-                        @else
-                            <div class="row mt-4">
-                                @foreach ($courses as $course)
-                                    <div class="col-lg-4 mt-4">
-                                        <div class="card rounded-3 overflow-hidden h-100">
-                                            {{-- <a href="#" class="position-relative">
+                        </div>
+                    @else
+                        <div class="row mt-4">
+                            @foreach ($courses as $course)
+                                <div class="col-lg-4 mt-4">
+                                    <div class="card rounded-3 overflow-hidden h-100">
+                                        {{-- <a href="#" class="position-relative">
                                     <img height="50%" width="50%"
                                     src="{{ $course->image_url ? asset('storage/' . $course->image_url) : asset('assets/images/frontend-pages/blog-3.jpg') }}"
                                         alt="{{ $course->title }}" class="w-100 img-fluid" />
                                 </a> --}}
-                                            <div class="mt-7 px-7 pb-7 h-100">
-                                                <div class="d-flex gap-3 flex-column h-100 justify-content-between">
-                                                    <a href="#" class="fs-5 fw-bolder">{{ $course->title }}
-                                                        {{ $course->level ? ' - ' . $course->level : '' }} </a>
-                                                    <p>{{ 'Category: ' . ($course->category?->name ?? 'Uncategorized') }}
-                                                    </p>
-                                                    <ul class="list-unstyled mb-0">
-                                                        <li class="mb-2 d-flex align-items-start gap-2">
-                                                            <iconify-icon icon="mdi:calendar-clock"
-                                                                class="text-primary fs-4 mt-1"></iconify-icon>
-                                                            <span class="text-dark fs-3">Duration:
-                                                                {{ $course->duration ?? 'N/A' }}</span>
-                                                        </li>
-                                                        <li class="mb-2 d-flex align-items-start gap-2">
-                                                            <iconify-icon icon="mdi:laptop"
-                                                                class="text-success fs-4 mt-1"></iconify-icon>
-                                                            <span class="text-dark fs-3">Mode:
-                                                                {{ ucfirst($course->mode) ?? 'N/A' }}</span>
-                                                        </li>
-                                                        <li class="mb-2 d-flex align-items-start gap-2">
-                                                            <iconify-icon icon="mdi:school-outline"
-                                                                class="text-warning fs-4 mt-1"></iconify-icon>
-                                                            <span class="text-dark fs-3">Level:
-                                                                {{ $course->level ?? 'N/A' }}</span>
-                                                        </li>
-                                                        <li class="d-flex align-items-start gap-2">
-                                                            <iconify-icon icon="mdi:certificate-outline"
-                                                                class="text-info fs-4 mt-1"></iconify-icon>
-                                                            <span class="text-dark fs-3">Certification:
-                                                                {{ $course->certification ?? 'N/A' }}</span>
-                                                        </li>
-                                                    </ul>
-                                                    <div class="mt-0" id="details-{{ $course->id }}">
-                                                        <ol class="list-unstyled">
-                                                            <p class="mb-1 text-primary"><strong>Prerequisites:</strong>
-                                                            </p>
-                                                            @foreach (explode("\n", $course->prerequisites ?? '') as $prerequisite)
-                                                                @if (trim($prerequisite) !== '')
-                                                                    <li class="d-flex align-items-start gap-2">
-                                                                        <i class="ti ti-check text-success mt-1"></i>
-                                                                        <span>{{ $prerequisite }}</span>
-                                                                    </li>
-                                                                @endif
-                                                            @endforeach
-                                                        </ol>
-                                                    </div>
+                                        <div class="mt-7 px-7 pb-7 h-100">
+                                            <div class="d-flex flex-column h-100 justify-content-between">
+                                                <a href="javascript:void(0);" class="fs-5 fw-bolder">{{ $course->title }}
+                                                    {{ $course->level ? ' - ' . $course->level : '' }} </a>
+                                                <p class="mt-1">Category: {{ $course->category?->name ?? 'Uncategorized' }}</p>
+                                                <ul class="list-unstyled mb-0">
+                                                    <li class="mb-2 d-flex align-items-start gap-2">
+                                                        <iconify-icon icon="mdi:calendar-clock"
+                                                            class="text-primary fs-4 mt-1"></iconify-icon>
+                                                        <span class="text-dark fs-3">Duration:
+                                                            {{ $course->duration ?? 'N/A' }}</span>
+                                                    </li>
+                                                    <li class="mb-2 d-flex align-items-start gap-2">
+                                                        <iconify-icon icon="mdi:laptop"
+                                                            class="text-success fs-4 mt-1"></iconify-icon>
+                                                        <span class="text-dark fs-3">Mode:
+                                                            {{ ucfirst($course->mode) ?? 'N/A' }}</span>
+                                                    </li>
+                                                    <li class="mb-2 d-flex align-items-start gap-2">
+                                                        <iconify-icon icon="mdi:school-outline"
+                                                            class="text-warning fs-4 mt-1"></iconify-icon>
+                                                        <span class="text-dark fs-3">Level:
+                                                            {{ $course->level ?? 'N/A' }}</span>
+                                                    </li>
+                                                    <li class="d-flex align-items-start gap-2">
+                                                        <iconify-icon icon="mdi:certificate-outline"
+                                                            class="text-info fs-4 mt-1"></iconify-icon>
+                                                        <span class="text-dark fs-3">Certification:
+                                                            {{ $course->certification ?? 'N/A' }}</span>
+                                                    </li>
+                                                </ul>
+                                                <div class="mt-0" id="details-{{ $course->id }}">
+                                                    <ol class="list-unstyled">
+                                                        <p class="mb-1 text-primary"><strong>Prerequisites:</strong>
+                                                        </p>
+                                                        @foreach (explode("\n", $course->prerequisites ?? '') as $prerequisite)
+                                                            @if (trim($prerequisite) !== '')
+                                                                <li class="d-flex align-items-start gap-2">
+                                                                    <i class="ti ti-check text-success mt-1"></i>
+                                                                    <span>{{ $prerequisite }}</span>
+                                                                </li>
+                                                            @endif
+                                                        @endforeach
+                                                    </ol>
+                                                </div>
 
-                                                    <div>
-                                                        <a class="btn btn-primary d-block w-100 mb-3"
-                                                            href="{{ route('front-end.course-application', ['course_id' => $course->id]) }}">Apply
-                                                            Now</a>
-                                                        @if ($course->brochure_url)
-                                                            <a href="{{ asset('storage/' . $course->brochure_url) }}"
-                                                                class="btn btn-outline-primary d-block w-100 mb-3"
-                                                                target="_blank">
-                                                                <i class="ti ti-download me-1"></i> Download Brochure
-                                                            </a>
-                                                        @endif
-                                                    </div>
+                                                <div>
+                                                    <a class="btn btn-primary d-block w-100 mb-3"
+                                                        href="{{ route('front-end.course-application', ['course_id' => $course->id]) }}">Apply
+                                                        Now</a>
+                                                    @if ($course->brochure_url)
+                                                        <a href="{{ asset('storage/' . $course->brochure_url) }}"
+                                                            class="btn btn-outline-primary d-block w-100 mb-3"
+                                                            target="_blank">
+                                                            <i class="ti ti-download me-1"></i> Download Brochure
+                                                        </a>
+                                                    @endif
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                @endforeach
-                            </div>
+                                </div>
+                            @endforeach
+                        </div>
                 </div>
 
                 @endif
