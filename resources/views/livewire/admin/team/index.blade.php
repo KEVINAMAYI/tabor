ea77890aa39c1af57b29258a390186959e6a77ca
ngugi_mukanga <dnmukanga@gmail.com>
2026-03-31 20:39:03 +0300

    more update on team image

--- resources/views/livewire/admin/team/index.blade.php
+++ resources/views/livewire/admin/team/index.blade.php
@@ -246,7 +246,7 @@ new class extends Component {
                             <!-- Image -->
                             <td>
                                 @if ($member->image)
-                                    <img src="{{ asset('storage/' . $member->image) }}" alt="{{ $member->name }}"
+                                    <img src="{{ url('team-image/' . basename($member->image)) }}" alt="{{ $member->name }}"
                                         class="rounded-circle" width="50" height="50">
                                 @else
                                     <span class="text-muted">No Image</span>
